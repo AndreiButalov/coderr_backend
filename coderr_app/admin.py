de0021e6c1475b14d58c1f0795428d6c1c6b3068
AndreiButalov <andrej.butalov@gmx.de>
2026-01-22 11:19:44 +0100

    add admin panel

--- coderr_app/admin.py
+++ coderr_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Offer, OfferDetail, Order, Review
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Offer)
+admin.site.register(OfferDetail)
+admin.site.register(Order)
+admin.site.register(Review)
