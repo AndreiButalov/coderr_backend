83d88aaca69f5f9a5cb397a76d54ff2dc8667a1b
AndreiButalov <andrej.butalov@gmx.de>
2026-02-02 08:55:09 +0100

    api/offers POST-> request->update-id delete

--- coderr_app/api/serializers.py
+++ coderr_app/api/serializers.py
@@ -140,27 +140,28 @@ class OfferSerializer(serializers.ModelSerializer):
         }
 
 
+
+class OfferDetailCreateSerializer(OfferDetailSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = OfferDetail
+        exclude = ['id', 'offer']
+
+
 class OfferCreateSerializer(serializers.ModelSerializer):
-    details = OfferDetailSerializer(many=True)
+    details = OfferDetailCreateSerializer(many=True)
 
     class Meta:
         model = Offer
         fields = ['title', 'image', 'description', 'details']
 
-    def validate_details(self, value):
-        if len(value) != 3:
-            raise serializers.ValidationError(
-                "Ein Angebot muss genau 3 Details enthalten."
-            )
-        return value
-
     def create(self, validated_data):
         details_data = validated_data.pop('details')
-        user = self.context['request'].user
-        offer = Offer.objects.create(user=user, **validated_data)
-        for detail_data in details_data:
-            OfferDetail.objects.create(offer=offer, **detail_data)
-
+        offer = Offer.objects.create(
+            user=self.context['request'].user,
+            **validated_data
+        )
+        for detail in details_data:
+            OfferDetail.objects.create(offer=offer, **detail)
         return offer
     
 
